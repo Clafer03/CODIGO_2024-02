f6b90aafcae394edb153ff2adf7dd26d5b99cd97
Clafer03 <claudi125.lol@gmail.com>
2024-09-29 13:16:33 -0500

    tarea y bot-msg

---
+++ Prog_Competitiva/tarea4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node * next;
+    Node * prev;
+    Node(int x){
+        data = x;
+        next=NULL;
+        prev=NULL;
+    }
+};
+
+Node* reverseDLL(Node * head, int n){
+
+}
+
+
+int main(){
+
+    int n, k;
+    cout << "-\n";
+    cin >> n;
+
+    reverseDLL()
+
+}
+
+// reporte
+// q postes diarios
+
+// ejecucion de obra 3 veces x sem
+// revision avance 2 veces x sem
+// sustentar
+
